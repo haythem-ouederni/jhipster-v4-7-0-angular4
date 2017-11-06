c7ef548459af76fe08a40aea798c9638fb3e483a
haythem ouederni <haythem.ouederni@gmail.com>
2017-11-06 22:19:56 +0100

    adding webpack-bundle-analyzer

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -3,6 +3,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const StringReplacePlugin = require('string-replace-webpack-plugin');
 const MergeJsonWebpackPlugin = require("merge-jsons-webpack-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const utils = require('./utils.js');
 
@@ -13,7 +14,9 @@ module.exports = (options) => {
     };
     return {
         resolve: {
-            extensions: ['.ts', '.js'],
+            extensions: [
+                '.ts', '.js'
+            ],
             modules: ['node_modules']
         },
         stats: {
@@ -21,34 +24,35 @@ module.exports = (options) => {
         },
         module: {
             rules: [
-                { test: /bootstrap\/dist\/js\/umd\//, loader: 'imports-loader?jQuery=jquery' },
                 {
+                    test: /bootstrap\/dist\/js\/umd\//,
+                    loader: 'imports-loader?jQuery=jquery'
+                }, {
                     test: /\.html$/,
                     loader: 'html-loader',
                     options: {
                         minimize: true,
                         caseSensitive: true,
-                        removeAttributeQuotes:false,
-                        minifyJS:false,
-                        minifyCSS:false
+                        removeAttributeQuotes: false,
+                        minifyJS: false,
+                        minifyCSS: false
                     },
                     exclude: ['./src/main/webapp/index.html']
-                },
-                {
+                }, {
                     test: /\.(jpe?g|png|gif|svg|woff2?|ttf|eot)$/i,
                     loaders: ['file-loader?hash=sha512&digest=hex&name=content/[hash].[ext]']
-                },
-                {
+                }, {
                     test: /manifest.webapp$/,
                     loader: 'file-loader?name=manifest.webapp!web-app-manifest-loader'
-                },
-                {
+                }, {
                     test: /app.constants.ts$/,
                     loader: StringReplacePlugin.replace({
-                        replacements: [{
-                            pattern: /\/\* @toreplace (\w*?) \*\//ig,
-                            replacement: (match, p1, offset, string) => `_${p1} = ${DATAS[p1]};`
-                        }]
+                        replacements: [
+                            {
+                                pattern: /\/\* @toreplace (\w*?) \*\//ig,
+                                replacement: (match, p1, offset, string) => `_${p1} = ${DATAS[p1]};`
+                            }
+                        ]
                     })
                 }
             ]
@@ -59,59 +63,76 @@ module.exports = (options) => {
                     'NODE_ENV': JSON.stringify(options.env)
                 }
             }),
-            new webpack.optimize.CommonsChunkPlugin({
-                name: 'polyfills',
-                chunks: ['polyfills']
-            }),
-            new webpack.optimize.CommonsChunkPlugin({
-                name: 'vendor',
-                chunks: ['main'],
-                minChunks: module => utils.isExternalLib(module)
-            }),
-            new webpack.optimize.CommonsChunkPlugin({
-                name: ['polyfills', 'vendor'].reverse()
-            }),
-            new webpack.optimize.CommonsChunkPlugin({
-                name: ['manifest'],
-                minChunks: Infinity,
-            }),
+            new webpack
+                .optimize
+                .CommonsChunkPlugin({name: 'polyfills', chunks: ['polyfills']}),
+            new webpack
+                .optimize
+                .CommonsChunkPlugin({
+                    name: 'vendor',
+                    chunks: ['main'],
+                    minChunks: module => utils.isExternalLib(module)
+                }),
+            new webpack
+                .optimize
+                .CommonsChunkPlugin({
+                    name: ['polyfills', 'vendor'].reverse()
+                }),
+            new webpack
+                .optimize
+                .CommonsChunkPlugin({name: ['manifest'], minChunks: Infinity}),
             /**
              * See: https://github.com/angular/angular/issues/11580
              */
-            new webpack.ContextReplacementPlugin(
-                /angular(\\|\/)core(\\|\/)@angular/,
-                utils.root('src/main/webapp/app'), {}
-            ),
+            new webpack.ContextReplacementPlugin(/angular(\\|\/)core(\\|\/)@angular/, utils.root('src/main/webapp/app'), {}),
             new CopyWebpackPlugin([
-                { from: './node_modules/core-js/client/shim.min.js', to: 'core-js-shim.min.js' },
-                { from: './node_modules/swagger-ui/dist/css', to: 'swagger-ui/dist/css' },
-                { from: './node_modules/swagger-ui/dist/lib', to: 'swagger-ui/dist/lib' },
-                { from: './node_modules/swagger-ui/dist/swagger-ui.min.js', to: 'swagger-ui/dist/swagger-ui.min.js' },
-                { from: './src/main/webapp/swagger-ui/', to: 'swagger-ui' },
-                { from: './src/main/webapp/favicon.ico', to: 'favicon.ico' },
-                { from: './src/main/webapp/manifest.webapp', to: 'manifest.webapp' },
+                {
+                    from: './node_modules/core-js/client/shim.min.js',
+                    to: 'core-js-shim.min.js'
+                }, {
+                    from: './node_modules/swagger-ui/dist/css',
+                    to: 'swagger-ui/dist/css'
+                }, {
+                    from: './node_modules/swagger-ui/dist/lib',
+                    to: 'swagger-ui/dist/lib'
+                }, {
+                    from: './node_modules/swagger-ui/dist/swagger-ui.min.js',
+                    to: 'swagger-ui/dist/swagger-ui.min.js'
+                }, {
+                    from: './src/main/webapp/swagger-ui/',
+                    to: 'swagger-ui'
+                }, {
+                    from: './src/main/webapp/favicon.ico',
+                    to: 'favicon.ico'
+                }, {
+                    from: './src/main/webapp/manifest.webapp',
+                    to: 'manifest.webapp'
+                },
                 // { from: './src/main/webapp/sw.js', to: 'sw.js' },
-                { from: './src/main/webapp/robots.txt', to: 'robots.txt' }
+                {
+                    from: './src/main/webapp/robots.txt',
+                    to: 'robots.txt'
+                }
             ]),
-            new webpack.ProvidePlugin({
-                $: "jquery",
-                jQuery: "jquery"
-            }),
+            new webpack.ProvidePlugin({$: "jquery", jQuery: "jquery"}),
             new MergeJsonWebpackPlugin({
                 output: {
                     groupBy: [
-                        { pattern: "./src/main/webapp/i18n/en/*.json", fileName: "./i18n/en.json" },
-                        { pattern: "./src/main/webapp/i18n/fr/*.json", fileName: "./i18n/fr.json" }
-                        // jhipster-needle-i18n-language-webpack - JHipster will add/remove languages in this array
+                        {
+                            pattern: "./src/main/webapp/i18n/en/*.json",
+                            fileName: "./i18n/en.json"
+                        }, {
+                            pattern: "./src/main/webapp/i18n/fr/*.json",
+                            fileName: "./i18n/fr.json"
+                        }
+                        // jhipster-needle-i18n-language-webpack - JHipster will add/remove languages in
+                        // this array
                     ]
                 }
             }),
-            new HtmlWebpackPlugin({
-                template: './src/main/webapp/index.html',
-                chunksSortMode: 'dependency',
-                inject: 'body'
-            }),
-            new StringReplacePlugin()
+            new HtmlWebpackPlugin({template: './src/main/webapp/index.html', chunksSortMode: 'dependency', inject: 'body'}),
+            new StringReplacePlugin(),
+            new BundleAnalyzerPlugin()
         ]
     };
 };
